691dfd45b17cbe6dab9123922f46a91bdb0c3a19
greencoffee1635 <166354@naver.com>
2021-10-11 23:38:43 +0900

    한 개의 이벤트 핸들러를 여러 Element에 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  return <div className="App">
-    <input onChange={(event)=>{
-      setInputValue(event.target.value)
-    }} />
-    <span>{inputValue}</span>
-  </div>;
+  const [person, setPerson] = useState({
+    name: "김오이",
+    drink: "HENDRICK'S"
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPerson((current) => {
+      const newPerson = {...current};
+      newPerson[name] = value;
+      return newPerson;
+    })
+  }
+  return (
+    <div>
+      <input name="name" value={person.name} onChange={handleChange} />
+      <input name="drink" value={person.drink} onChange={handleChange} />
+      <button onClick={() => {
+        alert(`${person.name}님은 ${person.drink}를 마십니다.`)
+      }}>클릭</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
