526a858a986985a13a958c078d09f4c24ac66ee5
greencoffee1635 <166354@naver.com>
2021-10-11 14:59:04 +0900

    State를 전달받는 Component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import React, { useState } from "react";
+import React, {useState} from 'react'
+import Greeting from "./components/Greeting"
 
 function App() {
-  const [person, setPerson] = useState({
-    name: "김민수",
-    count: 0,
-  });
-  return (
-    <div className="App">
-      <button
-        onClick={() =>
-          setPerson(current => {
-            const newPerson = { ...current }
-            current.count = current.count + 1;
-            return newPerson;
-          })
-        }>클릭</button>
-      <span>
-        {person.name}님이 버튼을 {person.count}회 클릭하였습니다.
-      </span>
+    const [username, setUsername] = useState("");
+    
+    return <div>
+        <input value={username} onChange={(event) => {
+        setUsername(event.target.value);}} />
+        <Greeting username={username} />
     </div>
-  );
 }
 
-// const App = () => {
-//   const [person, setPerson] = useState({
-//       name: "민수",
-//       age: 23
-//   });
-// //  (...) 잘못된 예
-//   setPerson((current) => {
-//       current.age = 24;
-//       return current;
-//   })
-// }
-// //  (...) 올바른 예
-//   setPerson((current) => {
-//       const newPerson = {...current}; // Spread syntax(전개구문)를 이용해 object를 복사.
-//       newPerson.age = 24;
-//       return newPerson;
-//   })
-
-
-export default App;
+export default App
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,19 +1,7 @@
 import React from "react";
 
-// 1.
-const Greeting = props => {
-    const { username } = props;
-    return <h1>{username}님 안녕하세요.</h1>;
+const Greeting = ({ username }) => {
+  return <h1>{username}님 안녕하세요.</h1>;
 };
 
-// 2.
-// const Greeting = (props) => {
-//     return <h1>
-//     {props.username}</h1>
-// }
-
-// 3.
-// const Greeting = ({ username }) => {
-//     return <h1>{username}</h1>
-// }
 export default Greeting;
