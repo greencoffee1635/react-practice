0689ed7ccd54977a6e961238e1ad36f0827ce732
greencoffee1635 <166354@naver.com>
2021-10-11 12:45:01 +0900

    Object를 갖는 State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [person, setPerson] = useState({
+    name: "김민수",
+    count: 0,
+  });
   return (
     <div className="App">
-      <span>{count}회 클릭하였습니다.</span>
-      <button onClick={() => {
-          setCount(count + 1);
-          }}>클릭</button>
+      <button
+        onClick={() =>
+          setPerson(current => {
+            const newPerson = { ...current }
+            current.count = current.count + 1;
+            return newPerson;
+          })
+        }>클릭</button>
+      <span>
+        {person.name}님이 버튼을 {person.count}회 클릭하였습니다.
+      </span>
     </div>
-      
   );
 }
+
+// const App = () => {
+//   const [person, setPerson] = useState({
+//       name: "민수",
+//       age: 23
+//   });
+// //  (...) 잘못된 예
+//   setPerson((current) => {
+//       current.age = 24;
+//       return current;
+//   })
+// }
+// //  (...) 올바른 예
+//   setPerson((current) => {
+//       const newPerson = {...current}; // Spread syntax(전개구문)를 이용해 object를 복사.
+//       newPerson.age = 24;
+//       return newPerson;
+//   })
+
 
 export default App;
