556c651094063ac1228ca28d4ea30dc991a21876
greencoffee1635 <166354@naver.com>
2021-10-13 02:14:28 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, { useState, useCallback } from "react";
+import React, { useRef } from "react";
 
 function App() {
-  const [foo, setFoo] = useState(0);
-  const [bar, setBar] = useState(0);
-
-  const calc = useCallback(() => {
-    return foo + bar;
-  }, [foo, bar]);
+  const inputRef = useRef();
 
   return (
     <div>
-      <input value={foo} onChange={(event) => {
-        setFoo(parseInt(event.target.value));
-        // parseInt는 string을 정수로 변환해준다.
-      }} />
-      <input value={bar} onChange={(event) => {
-        setBar(parseInt(event.target.value));
-      }} />
-      <div>{calc()}</div>
-      {/* 함수니까 호출을 한번해서 리턴된 값을 출력해야한다. */}
+      <input ref={inputRef} />
+      <button onClick={() => {
+        alert(inputRef.current.value)
+      }}>클릭</button>
     </div>
-  );
+  )
 }
 
 export default App;
