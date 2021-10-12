a66677c640273816a4df4459c2d4790e8bdb8160
greencoffee1635 <166354@naver.com>
2021-10-13 02:07:25 +0900

    useCallback 사용하여 덧셈 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useCallback } from "react";
 
 function App() {
   const [foo, setFoo] = useState(0);
   const [bar, setBar] = useState(0);
-  const multi = useMemo(() => {
-    return foo * bar;
+
+  const calc = useCallback(() => {
+    return foo + bar;
   }, [foo, bar]);
+
   return (
     <div>
       <input value={foo} onChange={(event) => {
         setFoo(parseInt(event.target.value));
+        // parseInt는 string을 정수로 변환해준다.
       }} />
       <input value={bar} onChange={(event) => {
         setBar(parseInt(event.target.value));
       }} />
-      <div>{multi}</div>
+      <div>{calc()}</div>
+      {/* 함수니까 호출을 한번해서 리턴된 값을 출력해야한다. */}
     </div>
   );
 }
